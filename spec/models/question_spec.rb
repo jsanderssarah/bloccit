f9dc3b36515843660d337f03d610b3330352867a
Sarah <jsanders.sarah@gmail.com>
2017-06-26 13:29:29 -0500

    Assignment 15 Rails: Models attempted

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:questions) { Questions.create! (title: "New Questions Title", body: "New Questions Body", resolved: boolean) }
+
+  describe "attributes" do
+    it "has title body and resolved attributes"
+      expect(questions).to have_attributes(title: "New Questions Title", body: "New Questions Body", resolved: boolean)
+    end
+  end
+end
