777e48fa08c0182da0a7c1fb1fb0ab27e61512f0
Sarah <jsanders.sarah@gmail.com>
2017-06-30 16:58:35 -0500

    Checkpoint 18 Rails: Introduction to CRUD complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,27 @@ require 'random_data'
     body: RandomData.random_paragraph
   )
 end
-post = Post.all
+posts = Post.all
 
 #create comments
 100.times do
   Comment.create!(
-    post: post.sample,
+    post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+Post.find_or_create_by!(
+  title: "This is a Unique Title",
+  body: "This is a test to see if this post shows up a single time."
+)
+
+post = Post.where(title: 'This is a Unique Title').first
+Comment.create!(
+  post: post,
+  body:  "This is a comment on the unique post"
+)
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
