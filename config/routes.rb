cb2f6b63f9af1885d405e4a723ca0c93d08c8413
Sarah <jsanders.sarah@gmail.com>
2017-07-10 22:11:54 -0500

    Checkpoint 24 Rails: Authentication - User Model complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :sponsored_post
   end
 
+  resources :users, only: [:new, :create]
+
   get 'about' => 'welcome#about'
   root 'welcome#index'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
