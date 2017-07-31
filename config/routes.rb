28e40c7d2c77b2c5db2c441292f26a66fcec08b9
Sarah <jsanders.sarah@gmail.com>
2017-07-31 14:52:48 -0500

    assignment 25 Rails: Authentication - Signing Up progressing

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
     #we pass resources :posts to the resources :topics block. This nests the post routes under the topic routes.
     resources :posts, except: [:index]
     resources :sponsored_post
+
   end
 
   resources :users, only: [:new, :create]
-
+  post 'users/confirm' => 'users#confirm'
   get 'about' => 'welcome#about'
   root 'welcome#index'
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
