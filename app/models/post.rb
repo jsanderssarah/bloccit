409057abd18f4453e8aa7cc2db239d9bcd589f39
Sarah <jsanders.sarah@gmail.com>
2017-08-10 13:39:17 -0500

    Assignment 31 Rails: Favorites progressing

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  after_create :create_favorite
 
   default_scope { order('rank DESC')}
    ## The default_scope will order all posts by their created_at date, in descending order, with the most recent posts displayed first
@@ -34,4 +35,6 @@ class Post < ApplicationRecord
   def favorite_for(post)
     favorites_where(post_id: post.id).first
   end
+
+
 end
